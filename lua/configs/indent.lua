f31db5d12236291f248a8816f47c2d12ce729112
Mark-Asuncion <mark.asuncion.000@gmail.com>
2023-06-09 14:10:16 +0800

    Switch to lazyvim

--- lua/configs/indent.lua
+++
@@ -1,7 +0,0 @@
-require("indent_blankline").setup {
-    show_end_of_line = true,
-    space_char_blankline = " ",
-    --  !needs treesitter!
-    show_current_context = true,
-    show_current_context_start = true,
-}
