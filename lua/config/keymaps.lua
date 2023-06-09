09eb4d2dbf9b0a87c6e8c04314f7b958d0bb94fd
Mark-Asuncion <mark.asuncion.000@gmail.com>
2023-06-09 19:02:50 +0800

    DISABLED:: some plugins ^| ADD::fugitive ^| MODIFIED:: some plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.g.mapleader=" "
+-- native keymaps
+vim.keymap.set({'n','i', 'v'},'<A-a>', '<Esc>',{})
+-- copy and pasting
+vim.keymap.set('v','<leader>y', '"+y',{})
+vim.keymap.set({'n','v'},'<leader>p', '"+p',{})
+vim.keymap.set('v','y','"0y',{})
+vim.keymap.set('v','Y','"0Y',{})
+vim.keymap.set('v','p','"0p',{})
+vim.keymap.set('v','P','"0P',{})
+-- buffer nav
+vim.keymap.set('n','<leader>n', ':bn<CR>',{})
+vim.keymap.set('n','<leader>P', ':bp<CR>',{})
+vim.keymap.set('n','<leader>w',':bw<CR>',{})
+-- quickfix navi
+vim.keymap.set('n','<C-j>',':cn<CR>',{})
+vim.keymap.set('n','<C-k>',':cp<CR>',{})
---
+++ lua/plugins/disable.lua
@@ -0,0 +1,7 @@
+return {
+  {"rcarriga/nvim-notify", enabled=false},
+  {"folke/noice.nvim", enabled=false},
+  {"zbirenbaum/copilot.lua", enabled=false},
+  {"zbirenbaum/copilot-cmp", enabled=false},
+  {"echasnovski/mini.animate", enabled=false},
+}
