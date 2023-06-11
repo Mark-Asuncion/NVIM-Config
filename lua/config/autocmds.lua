6433f575408ebe22d05c1bff4cc7f824162053dd
Mark-Asuncion <mark.asuncion.000@gmail.com>
2023-06-11 22:05:55 +0800

    autocmd: change comment string of cpp,c,hpp,h files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.cmd([[
+augroup set-commentstring-ag
+    autocmd!
+    autocmd BufEnter *.cpp,*c,*hpp,*.h :lua vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
+    autocmd BufFilePost *.cpp,*c,*hpp,*.h :lua vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
+augroup END
+]])
