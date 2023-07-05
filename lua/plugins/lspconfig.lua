bc6a6d52bc89f83ea04b98d156cc7207d325f552
Mark-Asuncion <mark.asuncion.000@gmail.com>
2023-07-05 03:27:57 -0400

    ADD: ensure installed

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,46 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    autoformat = false,
-  },
+    "neovim/nvim-lspconfig",
+    opts = {
+        autoformat = false,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "bash",
+                "c",
+                "html",
+                "css",
+                "javascript",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "regex",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "yaml",
+                "cpp",
+                "comment",
+            },
+        },
+    },
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ensure_installed = {
+                "stylua",
+                "shfmt",
+                "clangd",
+                "html-lsp",
+                "css-lsp",
+                "typescript-language-server",
+                "black",
+                "prettier",
+            },
+        },
+    },
 }
