6d24147f539a68c4a488cc6b60608fc22dcaa886
Mark-Asuncion <mark.asuncion.000@gmail.com>
2023-07-07 03:31:42 -0400

    ADD: vscode neovim

--- init.lua
+++ init.lua
@@ -1,1 +1,9 @@
-require("config.lazy")
+if vim.g.vscode then
+    -- vim.keymap.set({'n','i', 'v'},'<A-a>', '<Esc>',{})
+    vim.opt.clipboard="unnamedplus"
+    vim.opt.scrolloff=10
+    vim.opt.incsearch=true
+    vim.opt.hlsearch=false
+else
+    require("config.lazy")
+end
