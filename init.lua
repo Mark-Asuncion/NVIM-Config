6fbbec70d0c34581a4627a88d6746f2cc6c09772
Mark-Asuncion <mark.asuncion.000@gmail.com>
2023-07-07 23:12:25 -0400

    remove vscode

--- init.lua
+++ init.lua
@@ -1,9 +1,1 @@
-if vim.g.vscode then
-    -- vim.keymap.set({'n','i', 'v'},'<A-a>', '<Esc>',{})
-    vim.opt.clipboard="unnamedplus"
-    vim.opt.scrolloff=10
-    vim.opt.incsearch=true
-    vim.opt.hlsearch=false
-else
-    require("config.lazy")
-end
+require("config.lazy")
