a5f5c11996ac1f22fc895f5e0694ca2722ca7ef8
Mark-Asuncion <mark.asuncion.000@gmail.com>
2023-09-06 21:26:58 +0800

    remove mini.animate

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -3,7 +3,6 @@ return {
   {"folke/noice.nvim", enabled=false},
   {"zbirenbaum/copilot.lua", enabled=false},
   {"zbirenbaum/copilot-cmp", enabled=false},
-  {"echasnovski/mini.animate", enabled=false},
   {"mfussenegger/nvim-dap", enabled=false},
   {"nvim-neotest/neotest", enabled=false},
   {"ahmedkhalf/project.nvim", enabled=false},
