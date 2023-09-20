ff8d838037abfba361604c66f60bc55e4c51db6e
Mark-Asuncion <mark.asuncion.000@gmail.com>
2023-09-20 13:13:11 +0800

    some changes

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,10 +1,13 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
-vim.cmd([[
-augroup set-commentstring-ag
-    autocmd!
-    autocmd BufEnter *.cpp,*c,*hpp,*.h :lua vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
-    autocmd BufFilePost *.cpp,*c,*hpp,*.h :lua vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
-augroup END
-]])
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(_)
+        vim.api.nvim_create_user_command("Fmt",
+            function() vim.lsp.buf.format() end
+                ,{})
+    end,
+})
+vim.api.nvim_create_autocmd({'BufEnter', 'BufFilePost' }, {
+    pattern = {"*.cpp", "*.c", "*.hpp", "*.h"},
+    callback = function(args)
+        vim.api.nvim_buf_set_option(args.buf, "commentstring", "// %s")
+    end,
+})
