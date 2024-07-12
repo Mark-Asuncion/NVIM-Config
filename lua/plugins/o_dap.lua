1eff63deaf329ac26fd546d5092bcde59188486d
Mark-Asuncion <mark.asuncion.000@gmail.com>
2024-07-12 22:33:02 +0800

    Fix Optional Pluging enabling function

--- lua/plugins/o_dap.lua
+++ lua/plugins/o_dap.lua
@@ -1,35 +1,35 @@
-local is_set = os.getenv("NVIM_DAP")
-if is_set ~= nil then
-    return {
-        "mfussenegger/nvim-dap",
-        dependencies = {
-            "jay-babu/mason-nvim-dap.nvim",
-            cmd = { "DapInstall", "DapUninstall" },
-            opts = {
-                -- A list of adapters to install if they're not already installed.
-                -- This setting has no relation with the `automatic_installation` setting.
-                ensure_installed = {},
+return {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "jay-babu/mason-nvim-dap.nvim",
+        cmd = { "DapInstall", "DapUninstall" },
+        enabled = function()
+            local is_set = os.getenv("NVIM_DAP")
+            return is_set ~= nil
+        end,
+        opts = {
+            -- A list of adapters to install if they're not already installed.
+            -- This setting has no relation with the `automatic_installation` setting.
+            ensure_installed = {},
 
-                -- NOTE: this is left here for future porting in case needed
-                -- Whether adapters that are set up (via dap) should be automatically installed if they're not already installed.
-                -- This setting has no relation with the `ensure_installed` setting.
-                -- Can either be:
-                --   - false: Daps are not automatically installed.
-                --   - true: All adapters set up via dap are automatically installed.
-                --   - { exclude: string[] }: All adapters set up via mason-nvim-dap, except the ones provided in the list, are automatically installed.
-                --       Example: automatic_installation = { exclude = { "python", "delve" } }
-                automatic_installation = true,
-                -- See below on usage
-                handlers = {},
-            }
-        },
-        config = function(_,_)
-            vim.api.nvim_create_user_command("DapScope", function()
-                local widgets = require('dap.ui.widgets')
-                widgets.sidebar(widgets.scopes).open()
-            end,{})
+            -- NOTE: this is left here for future porting in case needed
+            -- Whether adapters that are set up (via dap) should be automatically installed if they're not already installed.
+            -- This setting has no relation with the `ensure_installed` setting.
+            -- Can either be:
+            --   - false: Daps are not automatically installed.
+            --   - true: All adapters set up via dap are automatically installed.
+            --   - { exclude: string[] }: All adapters set up via mason-nvim-dap, except the ones provided in the list, are automatically installed.
+            --       Example: automatic_installation = { exclude = { "python", "delve" } }
+            automatic_installation = true,
+            -- See below on usage
+            handlers = {},
+        }
+    },
+    config = function(_,_)
+        vim.api.nvim_create_user_command("DapScope", function()
+            local widgets = require('dap.ui.widgets')
+            widgets.sidebar(widgets.scopes).open()
+        end,{})
 
-        end
-    }
-end
-return {}
+    end
+}
--- lua/plugins/o_tailwind.lua
+++ lua/plugins/o_tailwind.lua
@@ -1,27 +1,26 @@
 -- Mason install tailwind
-local is_set = os.getenv("NVIM_TAILWIND")
-if is_set ~= nil then
-    return {
-        "luckasRanarison/tailwind-tools.nvim",
-        dependencies = { "nvim-treesitter/nvim-treesitter" },
-        opts = {
-            document_color = {
-                enabled = true, -- can be toggled by commands
-                kind = "background", -- "inline" | "foreground" | "background"
-                inline_symbol = "󰝤 ", -- only used in inline mode
-                debounce = 200, -- in milliseconds, only applied in insert mode
+return {
+    "luckasRanarison/tailwind-tools.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    enabled = function()
+        local is_set = os.getenv("NVIM_TAILWIND")
+        return is_set ~= nil
+    end,
+    opts = {
+        document_color = {
+            enabled = true, -- can be toggled by commands
+            kind = "background", -- "inline" | "foreground" | "background"
+            inline_symbol = "󰝤 ", -- only used in inline mode
+            debounce = 200, -- in milliseconds, only applied in insert mode
+        },
+        conceal = {
+            enabled = false, -- can be toggled by commands
+            min_length = nil, -- only conceal classes exceeding the provided length
+            symbol = "󱏿", -- only a single character is allowed
+            highlight = { -- extmark highlight options, see :h 'highlight'
+                fg = "#38BDF8",
             },
-            conceal = {
-                enabled = false, -- can be toggled by commands
-                min_length = nil, -- only conceal classes exceeding the provided length
-                symbol = "󱏿", -- only a single character is allowed
-                highlight = { -- extmark highlight options, see :h 'highlight'
-                    fg = "#38BDF8",
-                },
-            },
-            custom_filetypes = {} -- see the extension section to learn how it works
-        }
+        },
+        custom_filetypes = {} -- see the extension section to learn how it works
     }
-end
-
-return {}
+}
